26f3ad022022277182649bac01a05178b8bfb588
h.ho <hosseinhosseiniat@gmail.com>
2025-06-01 21:54:37 +0330

    Create the project

---
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.bmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bmi.ui.theme.BMITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BMITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BmiApp(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BmiApp(innerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+
+    }
+}
+
